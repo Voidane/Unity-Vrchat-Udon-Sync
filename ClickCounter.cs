d50a8abc299090d7af60046f01e7d285bedbbe43
Voidane <53002511+Voidane@users.noreply.github.com>
2021-06-19 07:09:45 -0400

    Added in tut scripts

---
+++ ClickCounter.cs
@@ -0,0 +1,37 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using UnityEngine.UI;
+
+public class ClickCounter : UdonSharpBehaviour
+{
+
+    public Text getCount;
+
+    [UdonSynced] public int totalCount;
+
+
+
+    public override void Interact()
+    {
+        if (!Networking.IsOwner(gameObject))
+        {
+            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+        }
+
+        totalCount++;
+        getCount.text = totalCount + "";
+
+        RequestSerialization();
+    }
+
+
+
+    public override void OnDeserialization()
+    {
+        getCount.text = totalCount + "";
+    }
+
+}
---
+++ TriggerSync.cs
@@ -0,0 +1,42 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class TriggerSync : UdonSharpBehaviour
+{
+
+    public GameObject changeThis;
+
+    [UdonSynced] bool isON;
+
+
+
+    public override void Interact()
+    {
+        
+        if (!Networking.IsOwner(gameObject))
+        {
+            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+        }
+
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "turnONorOFF");
+
+    }
+
+
+
+    public void turnONorOFF()
+    {
+        isON = !changeThis.activeSelf;
+        changeThis.SetActive(isON);
+    }
+
+
+
+    public override void OnDeserialization()
+    {
+        changeThis.SetActive(isON);
+    }
+}
